1fbf87d74d64ef3c1f2602d90fc0aa65c05edcbf
madhawaucsc2453 <2020cs157@stu.ucsc.cmb.ac.lk>
2023-09-27 15:10:34 +0530

    git committed

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,5 +1,7 @@
 import * as UserViews from '../constants/AllViews';
 import React from 'react';
+
+//check madhawa
 export const customerRoutes = [
 	{id: 1, path: '/home', element: <UserViews.CustomerDashboard />},
 	{id: 2, path: '/packages', element: <UserViews.CustomerPackages />},
