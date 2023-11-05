2f7c2696148615fdc5b3036edf698dd86cc3cddb
Dilanka2000 <shdheshara20@gmail.com>
2023-11-05 22:51:37 +0530

    Staff

--- src/api/ApiEndpoints.js
+++ src/api/ApiEndpoints.js
@@ -2,3 +2,5 @@ export const SIGNIN_URL = '/api/signinuser';
 export const SIGNUP_URL = '/api/registeruser';
 export const VERIFY_USER_URL = '/api/verifyuser';
 export const ADD_PACKAGE_URL = '/api/addpackage'
+export const GET_CUSTOMERS_URL = '/api/customers'
+export const ADD_CUSTOMER_URL = '/api/addcustomer'
